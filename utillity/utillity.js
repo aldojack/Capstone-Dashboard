19cbc4a48219a9257a8806a3481055154421d165
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-06-30 12:31:38 +0100

    Imported create element utillity

---
+++ utillity/utillity.js
@@ -0,0 +1,17 @@
+export function createElement(type, options = {}) {
+    const element = document.createElement(type);
+
+    Object.entries(options).forEach(([key, value]) => {
+
+        if (key === 'class') {
+            value.forEach((className) => element.classList.add(className))
+            return;
+        }
+
+        if (key === 'text') {
+            element.textContent = value;
+        }
+        element.setAttribute(key, value);
+    })
+    return element;
+}
